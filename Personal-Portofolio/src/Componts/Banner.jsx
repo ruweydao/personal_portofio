cc7048a8c5437424dd12da4e3f4468a2dbd27c5a
ruweydao <165528513+ruweydao@users.noreply.github.com>
2024-03-31 03:57:02 +0300

    Add files via upload
    Ruwyda

---
+++ Personal-Portofolio/src/Componts/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='container'>
+    
+      <section className="hero-section">
+          <h2>
+        
+           <img className='imgs' src="./public/image07.png" alt=""/>
+            
+            <br />
+           
+            I'm Ruweyda,
+            <br />
+          Web Devalober
+          </h2>
+          <p>
+            Your Are 7 Years Exprense  Web Devalober Vision into Reality:
+            Innovative Digital Markating
+            by a Professional  Web Devalober And HR!
+          </p>
+          <button>Start Project</button>
+        </section>
+    </div>
+  )
+}
---
+++ Personal-Portofolio/src/Componts/Project.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Project() {
+  return (
+    <div className='Projcet' id='Project'>
+          
+      <h1>Projects</h1>
+      <div id='Items'>
+         <div id='item'>
+         <img src="image2.jpg" alt="" />
+  
+         <h4>React Project</h4>
+        
+         </div>
+
+         <div id='item'>
+         <img src="image3.jpg" alt="" />
+         <h4> Project </h4>
+         
+         </div>
+         <div id='item'>
+         <img src="Image4.jpg" alt="" />
+         <h4> Java Advanced </h4>
+       
+         </div>
+
+         <div id='item'>
+         <img src="Bank.png" alt="" />
+         <h4>Bank Project</h4>
+         </div>
+         
+      </div>
+    </div>
+  )
+}
